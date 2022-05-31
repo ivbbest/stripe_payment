d99b9f847b1a27eb90327038d3d8b8ccc95f8dac
Vladimir Irkhin <ivb1989@yandex.ru>
2022-05-31 16:16:07 +0300

    Добавление просмотра Django Моделей в Django Admin панели.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    """
+    Добавление товаров для просмотра в админке сайта
+    """
+    list_display = ('name', 'description', 'price',)
+    list_filter = ('price', 'name',)
+
+
+admin.site.register(Item, ItemAdmin)
