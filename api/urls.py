d40560b9127527dd8689a777fa95ee43411bf5c2
Vladimir Irkhin <ivb1989@yandex.ru>
2022-05-31 16:16:37 +0300

    Update urls.py.

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path('api/v1/buy/', ItemView.as_view()),
-    # path('test/', test_payment),
-    path('buy/<int:pk>', ItemView.as_view()),
-    path('buy/', ItemView.as_view()),
-    # path('item/<int:item_pk>', get_item),
+    path('buy/<int:pk>', BuyItemView.as_view()),
+    path('buy/', BuyItemView.as_view()),
+    path('item/<int:pk>', ItemView.as_view()),
 
-    # path('api/v1/item/<int>:id', item, name='item'),
 ]
