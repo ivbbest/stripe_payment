99f7bb4c2232f0f37553090d49feb27fe9c599be
Vladimir Irkhin <ivb1989@yandex.ru>
2022-05-31 16:32:22 +0300

    Улучшение файла models.py

--- api/models.py
+++ api/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(unique=True, max_length=255)
-    description = models.TextField(blank=True)
-    price = models.IntegerField(default=0)
+    name = models.CharField(unique=True, max_length=255, verbose_name='Product')
+    description = models.TextField(blank=True, verbose_name='Description')
+    price = models.IntegerField(default=0, verbose_name='Price')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
